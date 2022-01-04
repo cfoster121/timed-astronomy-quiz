b003aab04e82956d107e148b181a007c25960587
Courtney Foster <cfoster649@gmail.com>
2022-01-03 21:43:21 -0800

    Initial html + JS with quiz question placeholders

---
+++ script.js
@@ -0,0 +1,65 @@
+
+let quizQuestions = [
+    {
+        question: "question 1 text",
+        answers: ["a", "b", "c"],
+        correct: "a"
+    }
+]
+
+//Number of Seconds on Timer
+var timeSec = 10
+var timerCount = document.getElementById('timer')
+var quiz = document.getElementById('quiz')
+var startButton = document.getElementById('start')
+var timeLeft = function(){
+    timeSec--;
+    if (timeSec >=0) {
+        timerCount.textContent = timeSec
+    }
+ }
+
+
+//Timer
+
+function startQuiz() {
+    setInterval(timeLeft, 1000)
+    timerCount.removeAttribute("style")
+    startButton.setAttribute("style", "display: none")
+    getQuestion()
+}
+
+function getQuestion() {
+    var currentQuestion = quizQuestions[0].question
+    var questionH2 = document.createElement("h2")
+    questionH2.textContent = currentQuestion;
+    quiz.appendChild(questionH2)
+
+    quizQuestions[0].answers.forEach((answer)=>{
+        var answerEl = document.createElement("button")
+        answerEl.textContent = answer;
+        quiz.appendChild(answerEl)
+    })
+}
+
+startButton.addEventListener("click", startQuiz)
+
+//Questions
+
+
+
+
+
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
+
+
